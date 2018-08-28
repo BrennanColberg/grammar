9c0e8b95f0a37b3774dd0f4e7e6c700e3114eb8e
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-28 14:21:43 -0700

    Formatting generate.php
    working towards #7

--- backend/generate.php
+++ backend/generate.php
@@ -1,3 +1,33 @@
 <?php {
 	
+#	generate.php -- GET used to generate phrases
+#		> grammar -- the grammar to use, corresponding to a folder within 	\"grammars" in the file navigator [math, japanese]
+#		> key -- the tag within the language with which to generate phrases
+#		> format -- the HTML tag within which to embed each phrase [p, a, div, section, li, ...] (if none, it won't be within a tag)
+#		> quantity -- the number of phrases to generate
+	
+	# loading the specified grammar
+	$grammar = $_GET["grammar"];
+	
+	# getting the key and validating that the grammar contains it
+	$key = $_GET["key"];
+	
+	# HTML format within which to output calculated value
+	$format = $_GET["format"];
+	$start_tag = $format ? "<$format>" : "";
+	$end_tag = $format ? "</$format>\n" : "";
+	
+	# number of times to calculate and output a new value
+	$quantity = $_GET["quantity"];
+	if (!$quantity) $quantity = 1;
+	
+	# iterates calculation and output of the appropriately generated value
+	for ($i = 0; $i < $quantity; $i++) {
+		print($start_tag);
+		
+		
+		
+		print($end_tag);
+	}
+	
 } ?>
