8931b813c53ac594574b06ac861e169b40e26d32
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-30 12:01:26 -0700

    Finished info.php
    - closed #6
    - removed "exists" mode because it didn't work and wasn't necessary
    - added "url" mode
    - parsed out full application returning JSON

--- backend/info.php
+++ backend/info.php
@@ -2,25 +2,21 @@
 	
 #	info.php -- GET used to get info about a grammar
 #	> mode -- defining which action to use
-#		= exists -- returns boolean value about the grammar existing
 #		= keys -- returns a JSON array of the grammar's keys, in strings
 #		= define -- returns JSON array of the key's definitions
 #	> grammar -- the grammar to get info about
 #	> key -- the key to get info about
 	
+	header('Content-Type: application/json');
+	
 	$mode = $_GET["mode"];
 	$grammar = $_GET["grammar"];
 	$key = $_GET["key"];
 	
-	if ($mode === "exists" && $grammar) {
-		
-		
-	} else if ($mode === "url" && $grammar) {
-		
-		
+	if ($mode === "url" && $grammar) {
+		print(json_encode("../grammars/$grammar/"));
 	} else if ($mode === "keys" && $grammar) {
 		include('grammarParser.php');
-		header('Content-Type: application/json');
 		$grammar_rules = parse_grammar($grammar);
 		if ($grammar_rules) {	# grammar must be valid
 			$keys = array_keys($grammar_rules);
@@ -28,7 +24,6 @@
 		}
 	} else if ($mode === "define" && $grammar && $key) {
 		include('grammarParser.php');
-		header('Content-Type: application/json');
 		$grammar_rules = parse_grammar($grammar);
 		if ($grammar_rules) {	# grammar must be valid
 			$definitions = $grammar_rules[$key];
