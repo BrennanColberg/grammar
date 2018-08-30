951abb4b436a4b8c1e2eece95b1ec8b1419e750c
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-30 11:15:46 -0700

    info.php redefinition & "keys" mode testing
    works towards #6 and changes work done in #5

--- backend/info.php
+++ backend/info.php
@@ -1,3 +1,29 @@
 <?php {
 	
+#	info.php -- GET used to get info about a grammar
+#	> mode -- defining which action to use
+#		= exists -- returns boolean value about the grammar existing
+#		= keys -- returns a JSON array of the grammar's keys, in strings
+#		= define -- returns JSON array of the key's definitions
+#	> grammar -- the grammar to get info about
+#	> key -- the key to get info about
+	
+	$mode = $_GET["mode"];
+	$grammar = $_GET["grammar"];
+	$key = $_GET["key"];
+	
+	if ($mode === "exists" && $grammar) {
+		
+		
+	} else if ($mode === "url" && $grammar) {
+		
+		
+	} else if ($mode === "keys" && $grammar) {
+		include('grammarParser.php');
+		header('Content-Type: application/json');
+		print(json_encode(array_keys(parse_grammar($grammar))));
+	} else if ($mode === "define" && $grammar && $key) {
+		
+	}
+	
 } ?>
