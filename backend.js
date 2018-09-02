5ca1b4ddf4940e074cc1a54122ed3fd01f4adcfd
Brennan Colberg <brennan.colberg@gmail.com>
2018-09-02 12:14:03 -0700

    "key" selector auto-populates in HTML
    #10

--- backend.js
+++ backend.js
@@ -3,6 +3,21 @@
 	
 	window.addEventListener("load", function() {
 		
+		ajaxGET("backend/info.php?grammar=english&mode=keys", loadKeyOptions);
+		
 	});
+	
+	function loadKeyOptions(json) {
+		let key = $("key");
+		console.log(json);
+		let data = JSON.parse(json);
+		console.log(data);
+		for (let i = 0; i < data.length; i++) {
+			let option = ce("option");
+			option.value = data[i];
+			option.textContent = data[i];
+			key.appendChild(option);
+		}
+	}
 	
 })();
