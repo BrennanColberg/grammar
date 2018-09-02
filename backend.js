e82ba1b21f33f6fa7b77041584ddf14efb2d1495
Brennan Colberg <brennan.colberg@gmail.com>
2018-09-02 12:46:57 -0700

    full backend JS linking & functionality
    - generate button works
    - grammar & key selectors auto-populate
    closed #10

--- backend.js
+++ backend.js
@@ -2,21 +2,48 @@
 (function() {
 	
 	window.addEventListener("load", function() {
-		
-		ajaxGET("backend/info.php?grammar=english&mode=keys", loadKeyOptions);
-		
+		$("grammar").onchange = selectGrammar;
+		$("key").onchange = generate;
+		$("generate").onclick = generate;
+		ajaxGET("backend/info.php?mode=list", loadGrammarOptions);
 	});
 	
+	function selectGrammar() {
+		ajaxGET("backend/info.php?mode=list&grammar=" + this.value, loadKeyOptions);
+	}
+	
+	function generate() {
+		let grammar = $("grammar").value;
+		console.log("grammar is " + grammar);
+		let key = $("key").value;
+		console.log("key is " + key);
+		let quantity = $("quantity").value;
+		console.log("quantity is " + quantity);
+		ajaxGET("backend/generate.php?format=li&grammar=" + grammar + "&key=" + key + "&quantity=" + quantity, function(html) {
+			$("output").innerHTML = html;
+		});
+	}
+	
+	function loadGrammarOptions(json) {
+		loadOptionsJSON($("grammar"), json);
+		ajaxGET("backend/info.php?mode=list&grammar=" + $("grammar").value, loadKeyOptions);
+	}
+	
 	function loadKeyOptions(json) {
-		let key = $("key");
-		console.log(json);
+		loadOptionsJSON($("key"), json);
+		generate();
+	}
+	
+	function loadOptionsJSON(select, json) {
+		while (select.firstChild) {
+			select.removeChild(select.firstChild);
+		}
 		let data = JSON.parse(json);
-		console.log(data);
 		for (let i = 0; i < data.length; i++) {
 			let option = ce("option");
 			option.value = data[i];
 			option.textContent = data[i];
-			key.appendChild(option);
+			select.appendChild(option);
 		}
 	}
 	
