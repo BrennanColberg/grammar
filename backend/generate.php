4955a55bd8424cd572fd44555088b29ce23fc230
Brennan Colberg <brennan.colberg@gmail.com>
2018-09-10 11:48:51 -0700

    Cleaned & commented generate.php

--- backend/generate.php
+++ backend/generate.php
@@ -1,56 +1,68 @@
-<?php {
-	
-#	generate.php -- GET used to generate phrases
-#		> grammar -- the grammar to use, corresponding to a folder within 	\"grammars" in the file navigator [math, japanese]
-#		> key -- the tag within the language with which to generate phrases
-#		> format -- the HTML tag within which to embed each phrase [p, a, div, section, li, ...] (if none, it won't be within a tag)
-#		> quantity -- the number of phrases to generate
-	
-	include("grammarParser.php");
-	
-	# loading the specified grammar into $grammar
-	$grammar = parse_grammar($_GET["grammar"]);
-	
-	# getting the key and validating that the grammar contains it
-	$key = $_GET["key"];
-	if (!in_array($key, array_keys($grammar))) {
-		exit();
-	}
-	
-	# HTML format within which to output calculated value
-	$format = $_GET["format"];
-	$start_tag = $format ? "<$format>" : "";
-	$end_tag = $format ? "</$format>\n" : "";
-	
-	# number of times to calculate and output a new value
-	$quantity = $_GET["quantity"];
-	if (!$quantity) $quantity = 1;
-	
-	# iterates calculation and output of the appropriately generated value
-	for ($i = 0; $i < $quantity; $i++) {
-		print($start_tag);	# starting HTML tag for each value
+<?php
 
-		$string = fulfill_tags($key, $grammar);
-		print($string);
-		
-		print($end_tag);	# ending HTML tag for each value
-	}
-	
-	function fulfill_tags($string, $grammar) {
-		$result = $string;
-		for ($i = 0; $i < count(array_keys($grammar)); $i++) {
-			$key = array_keys($grammar)[$i];
-			while (strpos($result, $key) !== false) {
-				$chosen_index = mt_rand(0, count($grammar[$key]) - 1);
-				$chosen_definition = $grammar[$key][$chosen_index];
-				$key_index = strpos($result, $key);
-				$prior_string = substr($result, 0, $key_index);
-				$latter_string = substr($result, $key_index + strlen($key), strlen($result) - strlen($key));
-				$result = $prior_string . $chosen_definition . $latter_string;
-				$result = fulfill_tags($result, $grammar);
-			}
+/**
+ * This PHP file is used by backend.js, by GET request, to generate values
+ * from a grammar and specified key. It uses a grammar model created by
+ * grammarParser.php to recursively replace keys with appropriate
+ * definitions, thereby generating interesting results in a very
+ * optimized fashion.
+**/
+
+include("grammarParser.php");
+
+# loading GET inputs
+$grammar = parse_grammar($_GET["grammar"]);
+$key = $_GET["key"];
+$quantity = $_GET["quantity"];
+$format = $_GET["format"];
+
+# checking that the grammar both exists and contains the given key
+if (!$grammar || !in_array($key, array_keys($grammar))) exit();
+# checks that a quantity has been specified, defaulting to 1
+if (!$quantity) $quantity = 1;
+# checks that a format has been specified, defaulting to "p" (<p>)
+if (!$format) $format = "p";
+
+# iterates calculation and output of the appropriately generated value
+for ($i = 0; $i < $quantity; $i++) {
+	print("<$format>");	# starting HTML tag for each value
+	print(fulfill_keys($key, $grammar)); # generated HTML content
+	print("</$format>\n");	# ending HTML tag for each value
+}
+
+# recursive function that takes in a string and continually replaces
+# each valid grammatical key with one of its definitions at random until
+# all keys have been fulfilled
+function fulfill_keys($string, $grammar) {
+	# iterates through each key in the grammar
+	for ($i = 0; $i < count(array_keys($grammar)); $i++) {
+		$key = array_keys($grammar)[$i];
+		# every time there's an instance of the key in the string...
+		while (strpos($string, $key) !== false) {
+			
+			### SELECTS VALID DEFINITION ###
+			# chooses one of the key's definitions at random
+			$definition_index = mt_rand(0, count($grammar[$key]) - 1);
+			$definition = $grammar[$key][$definition_index];
+			# recursively replaces any keys within the new definition
+			# with their definitions, before moving on to use it in
+			# the final resulting string
+			$definition = fulfill_tags($definition, $grammar);
+			
+			### REPLACES KEY WITH DEFINITION ###
+			# finds the index where the key exists in the string
+			$key_index = strpos($string, $key);
+			# separates out the parts of the string before and after the
+			# key's first instance
+			$prior_string = substr($string, 0, $key_index);
+			$latter_string = substr($string, $key_index + strlen($key),
+					strlen($string) - strlen($key));
+			# inserts definition in the key's old place
+			$string = $prior_string . $definition . $latter_string;
+			
 		}
-		return $result;
 	}
-	
-} ?>
+	return $string;
+}
+
+?>
